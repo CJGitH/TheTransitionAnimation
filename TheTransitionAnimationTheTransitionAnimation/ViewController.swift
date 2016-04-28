544010037c22d4096f03ff4a84f996ed398efb57
CJGitH <2392777628@qq.com>
2016-04-28 09:38:45 +0800

    基本设置

---
+++ TheTransitionAnimationTheTransitionAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheTransitionAnimation
+//
+//  Created by chen on 16/4/27.
+//  Copyright © 2016年 chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
